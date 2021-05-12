7390e1dcca8b7eed3cb20c0946fed9fe63351e4e
thang241190 <thangnguyen24111990@gmail.com>
2021-05-12 15:20:54 +0300

    Finish part0 and part1

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,48 @@
+import React from "react"
+
+const App = () => {
+	// const-definitions
+	const course = "Half Stack application development"
+	const part1 = "Fundamentals of React"
+	const exercises1 = 10
+	const part2 = "Using props to pass data"
+	const exercises2 = 7
+	const part3 = "State of a component"
+	const exercises3 = 14
+
+	const total = exercises1 + exercises2 + exercises3
+	return (
+		<div>
+			<Header course={course} />
+			<Content
+				parts={[part1, part2, part3]}
+				exercises={[exercises1, exercises2.exercises3]}
+			/>
+			<Total total={total} />
+		</div>
+	)
+}
+
+const Header = ({ course }) => {
+	return <h1>{course}</h1>
+}
+const Content = ({ parts, exercises }) => (
+	<div>
+		<Part part={parts[0]} exercise={exercises[0]} />
+		<Part part={parts[1]} exercise={exercises[1]} />
+		<Part part={parts[2]} exercise={exercises[2]} />
+	</div>
+)
+
+const Part = ({ part, exercise }) => {
+	return (
+		<p>
+			{part} {exercise}
+		</p>
+	)
+}
+const Total = ({ total }) => {
+	return <p>Number of exercises {total}</p>
+}
+
+export default App
