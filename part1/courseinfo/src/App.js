0d243b7c01d9255924f1dbdda658bcb1879f0781
thang241190 <thangnguyen24111990@gmail.com>
2021-05-13 16:26:32 +0300

    Finish part1

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,22 +1,49 @@
 import React from "react"
 
+class Person {
+	constructor(name, age) {
+		this.name = name
+		this.age = age
+	}
+	greet() {
+		console.log("hello, my name is " + this.name)
+	}
+}
+
+const adam = new Person("Adam Ondra", 35)
+adam.greet()
+
+const janja = new Person("Janja Garnbret", 22)
+janja.greet()
+
 const App = () => {
 	// const-definitions
 	const course = "Half Stack application development"
-	const part1 = "Fundamentals of React"
-	const exercises1 = 10
-	const part2 = "Using props to pass data"
-	const exercises2 = 7
-	const part3 = "State of a component"
-	const exercises3 = 14
-
-	const total = exercises1 + exercises2 + exercises3
+	const parts = [
+		{
+			name: "Fundamentals of React",
+			exercises: 10,
+		},
+		{
+			name: "Using props to pass data",
+			exercises: 7,
+		},
+		{
+			name: "State of a component",
+			exercises: 14,
+		},
+	]
+	const total = parts[0].exercises + parts[1].exercises + parts[2].exercises
 	return (
 		<div>
 			<Header course={course} />
 			<Content
-				parts={[part1, part2, part3]}
-				exercises={[exercises1, exercises2.exercises3]}
+				parts={[parts[0].name, parts[1].name, parts[2].name]}
+				exercises={[
+					parts[0].exercises,
+					parts[1].exercises,
+					parts[2].exercises,
+				]}
 			/>
 			<Total total={total} />
 		</div>
