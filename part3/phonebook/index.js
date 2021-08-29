e0073842772be684366a2e54c3181c1118e71d2d
Thang Nguyen <thangnguyen24111990@gmail.com>
2021-08-29 15:52:26 +0300

    Finish ex3.1-3.6

---
+++ part3/phonebook/index.js
@@ -0,0 +1,87 @@
+const http = require("http")
+const express = require("express")
+const app = express()
+let persons = [
+	{
+		id: 1,
+		name: "Arto Hellas",
+		number: "040-123456",
+	},
+	{
+		id: 2,
+		name: "Ada Lovelace",
+		number: "39-44-5323523",
+	},
+	{
+		id: 3,
+		name: "Dan Abramovssss",
+		number: "12-43-234345",
+	},
+	{
+		id: 4,
+		name: "Mary Poppendieck1",
+		number: "39-23-6423122",
+	},
+]
+
+app.get("/api/persons", (request, response) => {
+	response.json(persons)
+})
+
+app.get("/info", (request, response) => {
+	const length = persons.length
+	const time = new Date()
+	response.send(
+		`<div><p>Phone book has info for ${length} people</p><p>${time}</p>`,
+	)
+})
+
+app.get("/api/persons/:id", (request, response) => {
+	const id = Number(request.params.id)
+	const person = persons.find((person) => {
+		return person.id === id
+	})
+	if (person) {
+		response.json(person)
+	} else {
+		response.status(404).end()
+	}
+})
+
+app.delete("/api/persons/:id", (request, response) => {
+	const id = Number(request.params.id)
+	persons = persons.filter((note) => note.id !== id)
+
+	response.status(204).end()
+})
+
+app.post("/api/persons", (request, response) => {
+	const body = request.query
+	const id = Math.floor(Math.random() * 10000)
+	if (!body.name) {
+		return response.status(400).json({
+			error: "Name is required",
+		})
+	}
+	if (!body.number) {
+		return response.status(400).json({
+			error: "Number is required",
+		})
+	}
+
+	if (persons.filter((person) => person.name === body.name)) {
+		return response.status(400).json({
+			error: "Name must be unique",
+		})
+	}
+
+	const newPersons = [
+		...persons,
+		{ id: id, name: body.name, number: body.number },
+	]
+	response.json(newPersons)
+})
+
+const PORT = 3001
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
