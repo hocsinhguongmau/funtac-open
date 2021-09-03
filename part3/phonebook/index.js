9f1b8b52e59284524b1c1833dde5ba1202f36758
Thang Nguyen <hsgm@Thangs-Mac-mini.local>
2021-09-04 01:33:48 +0300

    Finish 3.15-3.18

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,107 +1,88 @@
-const morgan = require("morgan")
-const express = require("express")
-const app = express()
-const cors = require("cors")
+const morgan = require("morgan");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+app.use(express.static("build"));
+app.use(express.json());
+require("dotenv").config();
 
 // app.use(morgan("tiny"))
 // morgan.token("body", (req) => JSON.stringify(req.body))
 
-app.use(cors())
+const Person = require("./models/person");
+
+app.use(cors());
 app.use(
-	morgan((tokens, req, res) => {
-		return [
-			tokens.method(req, res),
-			tokens.url(req, res),
-			tokens.status(req, res),
-			tokens.res(req, res, "content-length"),
-			"-",
-			tokens["response-time"](req, res),
-			"ms",
-			JSON.stringify(req.body),
-		].join(" ")
-	}),
-)
-let persons = [
-	{
-		id: 1,
-		name: "Arto Hellas",
-		number: "040-123456",
-	},
-	{
-		id: 2,
-		name: "Ada Lovelace",
-		number: "39-44-5323523",
-	},
-	{
-		id: 3,
-		name: "Dan Abramovssss",
-		number: "12-43-234345",
-	},
-	{
-		id: 4,
-		name: "Mary Poppendieck1",
-		number: "39-23-6423122",
-	},
-]
+  morgan((tokens, req, res) => {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, "content-length"),
+      "-",
+      tokens["response-time"](req, res),
+      "ms",
+      JSON.stringify(req.body),
+    ].join(" ");
+  })
+);
 
 app.get("/api/persons", (request, response) => {
-	response.json(persons)
-})
+  Person.find({}).then((persons) => {
+    response.json(persons);
+  });
+});
 
 app.get("/info", (request, response) => {
-	const length = persons.length
-	const time = new Date()
-	response.send(
-		`<div><p>Phone book has info for ${length} people</p><p>${time}</p>`,
-	)
-})
+  const length = persons.length;
+  const time = new Date();
+  response.send(
+    `<div><p>Phone book has info for ${length} people</p><p>${time}</p>`
+  );
+});
 
-app.get("/api/persons/:id", (request, response) => {
-	const id = Number(request.params.id)
-	const person = persons.find((person) => {
-		return person.id === id
-	})
-	if (person) {
-		response.json(person)
-	} else {
-		response.status(404).end()
-	}
-})
+app.put("/api/persons/:id", (request, response, next) => {
+  const id = request.params.id;
+  Person.findById(id)
+    .then((person) => {
+      if (person) {
+        response.json(person.toJSON());
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
+});
 
-app.delete("/api/persons/:id", (request, response) => {
-	const id = Number(request.params.id)
-	persons = persons.filter((note) => note.id !== id)
-
-	response.status(204).end()
-})
+app.delete("/api/persons/:id", (request, response, next) => {
+  const id = request.params.id;
+  Person.findByIdAndRemove(id)
+    .then(() => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
+});
 
 app.post("/api/persons", (request, response) => {
-	const body = request.query
-	const id = Math.floor(Math.random() * 10000)
-	if (!body.name) {
-		return response.status(400).json({
-			error: "Name is required",
-		})
-	}
-	if (!body.number) {
-		return response.status(400).json({
-			error: "Number is required",
-		})
-	}
+  const { body } = request;
+  if (!body.name) {
+    return response.status(400).json({
+      error: "Name is required",
+    });
+  }
+  if (!body.number) {
+    return response.status(400).json({
+      error: "Number is required",
+    });
+  }
 
-	if (persons.find((person) => person.name === body.name)) {
-		return response.status(400).json({
-			error: "Name must be unique",
-		})
-	}
+  const person = new Person({ name: body.name, number: body.number });
 
-	const newPersons = [
-		...persons,
-		{ id: id, name: body.name, number: body.number },
-	]
-	response.json(newPersons)
-})
+  person.save().then((savedPerson) => {
+    response.json(savedPerson);
+  });
+});
 
-const PORT = process.env.PORT || 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+const PORT = process.env.PORT;
+app.listen(PORT);
+console.log(`Server running on port ${PORT}`);
